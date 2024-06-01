742d7465603cd0b5e44f7c2d944fe48930351ce0
спс никитос <163382818+semyonov1511@users.noreply.github.com>
2024-06-01 14:43:20 +0300

    get-set functions for Work class added

--- Coursework/src/main/java/Entities/Work.java
+++ Coursework/src/main/java/Entities/Work.java
@@ -1,8 +1,59 @@
 package Entities;
 
 public class Work {
+
     public String name;
     public int standartTime;
     public int workersQuantity;
+    public String type;
+    public String part;
+    public double depth;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStandartTime() {
+        return standartTime;
+    }
+
+    public int getWorkersQuantity() {
+        return workersQuantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
     
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStandartTime(int standartTime) {
+        this.standartTime = standartTime;
+    }
+
+    public void setWorkersQuantity(int workersQuantity) {
+        this.workersQuantity = workersQuantity;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPart(String part) {
+        this.part = part;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
 }
